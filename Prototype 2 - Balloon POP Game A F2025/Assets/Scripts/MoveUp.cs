dc46dc8b92becbf4b5fee7994a19de7bde79761a
Templar2020 <preston.powell@uvu.edu>
2025-09-23 13:34:54 -0600

    SpawnManager, Player Penalty, Delete Effect

--- Prototype 2 - Balloon POP Game A F2025/Assets/Scripts/MoveUp.cs
+++ Prototype 2 - Balloon POP Game A F2025/Assets/Scripts/MoveUp.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class MoveUp : MonoBehaviour
 {
     public float speed;
+    public float upperBound = 15.0f; // Set an upper bound for ballloons to pass and for penalties to be applied
+    public ScoreManager scoreManager; // Referencing the Score Manager
+     public Balloon balloon; // Reference the balloon script to get the score
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        balloon = GetComponent<Balloon>();
     }
 
     // Update is called once per frame
@@ -17,5 +22,11 @@ public class MoveUp : MonoBehaviour
     {
         // Move the balloon up on the y axis at a set rate of speed
         transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (transform.position.y >= upperBound)
+        {
+            scoreManager.DecreaseScoreText(balloon.scoreToGive); //Reduce score for allowing the balloon to leave the screen
+            Destroy(gameObject); //pop the ballon after it leaves the screen
+        }
     }
 }
--- Prototype 2 - Balloon POP Game A F2025/Assets/Scripts/SpawnManager.cs
+++ Prototype 2 - Balloon POP Game A F2025/Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,7 @@ public class SpawnManager : MonoBehaviour
     public float startDelay = 0.5f;
     public float spawnInterval = 1.5f;
     public float xRange = 10.0f;
+    public float yPos = -10.0f;
 
 
     // Start is called before the first frame update
@@ -20,7 +21,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomBalloon()
     {
         // Get a random position on the x-axis
-        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), 0, 0);
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange),yPos, 0);
         // Pick a random balloon from the balloon array
         int balloonIndex = Random.Range(0, balloonPrefabs.Length);
         // Spawn random balloon at the spawn point
